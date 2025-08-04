8b595b3a0e4c26f717c9e4d5f79ab95a02cd9afe
Robin <robindev2019@outlook.com>
2025-08-04 07:51:51 +0200

    Prepare for publishing to Hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,35 @@
 defmodule Jinja.MixProject do
   use Mix.Project
 
+  @documentation "https://hexdocs.pm/jinja"
+  @git_repository "https://git.dupunkto.org/~axcelott/jinja"
+
   def project do
     [
+      name: "Jinja2",
       app: :jinja,
       version: "0.0.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      source_url: @git_repository,
+      homepage_url: @documentation,
+      description: description(),
+      package: package(),
+      docs: docs()
+    ]
+  end
+
+  def description do
+    "Jinja bindings for Elixir using Pythonx"
+  end
+
+  defp package do
+    [
+      licenses: ["Unlicense"],
+      links: %{"Sources" => @git_repository}
     ]
   end
 
@@ -20,7 +42,17 @@ defmodule Jinja.MixProject do
   defp deps do
     [
       {:structo, "~> 0.2.0"},
-      {:pythonx, "~> 0.4.5"}
+      {:pythonx, "~> 0.4.5"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Jinja",
+      api_reference: false,
+      authors: ["Robijntje"],
+      formatters: ["html"]
     ]
   end
 end
